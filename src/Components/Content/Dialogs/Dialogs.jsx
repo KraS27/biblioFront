f8646985d31eb6e4063d80dc35f778e205d9c2f1
KraSS <groverkras@gmail.com>
2023-02-08 18:22:37 +0200

    Customise Messages

--- src/Components/Content/Dialogs/Dialogs.jsx
+++ src/Components/Content/Dialogs/Dialogs.jsx
@@ -7,11 +7,13 @@ const Dialogs = (props) => {
     return (
         <div className={style.dialogs}>
             <div className={style.chats}>
+                <h2>Chat</h2>
                 <Chat text={"Vlad"} id={"1"} />
                 <Chat text={"Yarik"} id={"2"}/>
                 <Chat text={"Artem"} id={"3"}/>
             </div>
             <div className={style.messages}>
+                <h2>Messages</h2>
                 <Message text={"Hello"}/>
                 <Message text={"Yo-yo Nigga"}/>
                 <Message text={"Slava Ukraini"}/>
