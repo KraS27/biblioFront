435cef2da804239ff25c66462811adb298baf186
KraSS <groverkras@gmail.com>
2023-03-15 19:25:23 +0200

    Refactor Users from Functional to Class component

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -2,7 +2,44 @@ import React from 'react';
 import  style from './Users.module.css'
 import axios  from "axios";
 
-const Users = (props) => {
+
+class Users extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.getUsers();
+    }
+
+    getUsers = () => {
+        if(this.props.users.length === 0){
+            axios.get("https://localhost:7079/User/GetUsers").then(response => {
+                debugger;
+                const data = response.data;
+                this.props.setUsers(data);
+            })
+        }
+    }
+
+    render = () => {
+        return(
+            this.props.users.map(user =>
+                <div className={style.usersWrapper} key={user.id}>
+                    <img className={style.avatar} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLC7Upmjq-jwWf1b--JqHP7Q3SD31AT6RJwQ&usqp=CAU"} alt="Avatar"/>
+                    <div className={style.userData}>
+                        <p>Name: {user.userName}</p>
+                        <p>Description: {user.description}</p>
+                        {user.followed
+                            ? <button className={style.followButton} onClick={() => { this.props.unfollow(user.id)}} >Unfollow</button>
+                            : <button className={style.followButton} onClick={() => { this.props.follow(user.id)}} >Follow</button>
+                        }
+                    </div>
+                </div>
+            )
+        )
+    }
+}
+
+/*const Users = (props) => {
 
     if(props.users.length === 0){
         axios.get("https://localhost:7079/User/GetUsers").then(response => {
@@ -31,6 +68,6 @@ const Users = (props) => {
             {users}
         </div>
     );
-};
+};*/
 
 export default Users;
