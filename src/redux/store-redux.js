4de19269ac7867d90deace4f22a4fae103fbe7dd
KraSS <groverkras@gmail.com>
2023-04-03 23:09:16 +0300

    Create authReducer

--- src/redux/store-redux.js
+++ src/redux/store-redux.js
@@ -2,11 +2,13 @@ import {combineReducers, legacy_createStore} from "redux";
 import profileReducer from "./Reducers/profileReducer";
 import dialogsReducer from "./Reducers/dialogsReducer";
 import usersReducer from "./Reducers/usersReducer";
+import authReducer from "./Reducers/authReducer";
 
 let reducers = combineReducers({
     profilePage:profileReducer,
     dialogsPage:dialogsReducer,
-    userPage:usersReducer
+    userPage:usersReducer,
+    auth:authReducer
 })
 
 let store = legacy_createStore(reducers);
